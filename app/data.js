ce3488af51c7b7cc13b0da89b37a8d41a3d2337f
Dieuwke81 <46078857+Dieuwke81@users.noreply.github.com>
2026-03-03 11:00:58 +0100

    Update data.js

--- app/data.js
+++ app/data.js
@@ -1182,7 +1182,7 @@ export const vehicleChecklist = [
 ];
 export const usefulLinks = [
  { name: 'WebComm (duurt even, heb geduld)', url: 'https://dmz-wbc-web02.connexxion.nl/WebComm/default.aspx?' },
- { name: 'Intranet Transdev', url: 'intranet.transdev.nl' },
+ { name: 'Intranet Transdev', url: 'https://intranet.transdev.nl/Account/Login?ReturnUrl=%2F' },
   { name: 'OVinfo App (Android)', url: 'https://play.google.com/store/apps/details?id=nl.skywave.ovinfo' },
   { name: 'OVinfo App (iOS)', url: 'https://apps.apple.com/nl/app/ovinfo/id1144468923' },
   { name: 'Bussie.app', url: 'https://www.bussie.app/' },
