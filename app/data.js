bc143333ef40c4bc76c90492a31c56874b36fb3b
Dieuwke81 <46078857+Dieuwke81@users.noreply.github.com>
2026-03-12 08:51:59 +0100

    Update data.js

--- app/data.js
+++ app/data.js
@@ -1207,7 +1207,9 @@ export const vehicleChecklist = [
 export const usefulLinks = [
  { name: 'WebComm (duurt even, heb geduld)', url: 'https://dmz-wbc-web02.connexxion.nl/WebComm/default.aspx?' },
  { name: 'Intranet Transdev', url: 'https://intranet.transdev.nl/' },
- { name: 'Afas Pocket (Android)', url: 'https://play.google.com/store/apps/details?id=nl.afas.pocket2' },
+{ name: 'Team Transdev chauffeur app (Android)', url: 'https://play.google.com/store/apps/details?id=com.transdev.teamtransdev' },
+
+  { name: 'Afas Pocket (Android)', url: 'https://play.google.com/store/apps/details?id=nl.afas.pocket2' },
   { name: 'Afas Pocket (IOS)', url: 'https://apps.apple.com/nl/app/afas-pocket/id1028433924' },
 
   { name: 'OVinfo App (Android)', url: 'https://play.google.com/store/apps/details?id=nl.skywave.ovinfo' },
