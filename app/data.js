3d500325e3cc659064c6e7c77e4dec844ca4c884
Dieuwke81 <46078857+Dieuwke81@users.noreply.github.com>
2026-05-11 09:28:50 +0200

    Update data.js

--- app/data.js
+++ app/data.js
@@ -1077,7 +1077,7 @@ Deze bussen niet zelf pakken!`
       { id: 'a8', text: 'Uitleg Viribus/ticketbox. (Zie docs)' },
       { id: 'a7', text: `Korte uitleg Webcomm.
 • Ruilen via webcomm (NIET toegestaan voor uitzendkrachten en dagcontractanten)
-• Snipperdag aanvragen via de holidayplanner op webcomm. Dit kan 2 maanden min 1 dag van te voren. Binnen 8 dagen moet via de planning geregeld worden. (geconcretiseerd betekent dat je buiten de garantie staat)
+• Snipperdag aanvragen via de holidayplanner op webcomm. Dit kan 2 maanden min 1 dag van te voren. Binnen 8 dagen moet via de planning geregeld worden. (GECONCRETISEERD betekent dat je buiten de garantie staat)
 • Een uitzendkracht en dagcontractant dient tot na 16.30 op webcomm te kijken voor de dag erna. De dienst kan tot die tijd nog wijzigen
 • Voor de R-diensten in een roulering geldt dat er 3 dagen van te voren (einde dag) een dienst op zou moeten staan.`},
       { id: 'a1', text: 'Kledingvoorschrift in orde (Zie docs)' },
